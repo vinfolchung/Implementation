a75e3de82f106bdf584099b4bbf62ffde2e5809d
vinfolchung <373085644@qq.com>
2016-03-25 16:45:45 +0800

    框架搭建

---
+++ Implementation/Implementation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Implementation
+//
+//  Created by 钟文锋 on 16/3/25.
+//  Copyright © 2016年 vinfol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Implementation/Implementation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Implementation
+//
+//  Created by 钟文锋 on 16/3/25.
+//  Copyright © 2016年 vinfol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
